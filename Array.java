d7f4eddd6daaf46c5db27762cc1f803fc118a6c8
enrvz <77559690+enrvz@users.noreply.github.com>
2021-01-17 10:51:33 +0800

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class Array {
+
+	public static void main(String[] args) {
+		//declare data type,identifier,index
+		int numbers[] = {1,2,3,4,5,6};
+		//you can add each elements with array
+	System.out.println(numbers [3] + numbers[1]); 
+		
+
+	}
+
+}
